f71d6a67313fd75caec6ba2f93c1ab7541f62c5c
Dmytro Kozhukhar <424523+dkozhukhar@users.noreply.github.com>
2023-07-16 11:33:13 +0300

    Update myGame.js

--- myGame.js
+++ myGame.js
@@ -1,9 +1,16 @@
+document.body.style.margin = "0";
+document.body.style.padding = "0";
+document.body.style.boxSizing = "border-box";
+document.body.style.fontFamily = "'Arial', sans-serif";
+
+
 var divButtons = document.createElement("div");
 divButtons.style.width = "100%";
 divButtons.style.height = "50px";
 divButtons.style.border = "1px solid black";
-divButtons.style.position = "sticky";
+divButtons.style.position = "relative";
 divButtons.style.top = "0";
+divButtons.style.boxSizing = "border-box";
 document.body.appendChild(divButtons);
 
 var button1 = document.createElement("button");
@@ -21,6 +28,12 @@ button2.onclick = function() {
 divButtons.appendChild(button1);
 divButtons.appendChild(button2);
 
+var divScreens = document.createElement("div");
+divScreens.style.border = "1px solid black";
+divScreens.style.width = "100%";
+divScreens.style.height = "calc(100vh - 50px)";
+divScreens.style.boxSizing = "border-box"; // Add this line
+document.body.appendChild(divScreens);
 
 var screens = [];
 
@@ -30,6 +43,7 @@ function createScreen(name) {
     screen.element = document.createElement("div");
     screen.element.id = name;
     screen.element.style.border = "1px solid black";
+    screen.element.style.boxSizing = "border-box";
     screen.element.style.width = "100%";
     screen.element.style.height = "100%";
     if (name === "screen1") {
@@ -45,7 +59,7 @@ function createScreen(name) {
         screen.element.appendChild(canvas);  
     }
     screen.element.innerHTML = name;
-    document.body.appendChild(screen.element);
+    divScreens.appendChild(screen.element);
     screens.push(screen);
   }
 
